286c83c98e5120fa53e1ce98a74c64bb3710c33b
Mahima Singh <58130384+mahimasingh1502@users.noreply.github.com>
2022-02-08 19:25:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,16 +33,41 @@ function calculateBMI() {
                             / 10000)).toFixed(2);
   
         if (bmi < 18.6) result.innerHTML =
-            `Hi <span>${name}</span> your BMI is : <span>${bmi}</span> Under Weight`;
+            `Hi <span>${name}</span> your BMI is : <span>${bmi}</span>. <br>
+            This BMI is considered Underweight.<br>
+            Being underweight may pose certain health risks, including nutrient deficiencies and hormonal changes.
+             Waist-to-hip ratio, waist-to-height ratio, and body fat percentage measurements can provide a more 
+             complete picture of any health risks. A person should consult with their healthcare provider and 
+             consider making lifestyle changes through healthy eating and fitness to improve their health indicators.
+            `;
   
         else if (bmi >= 18.6 && bmi < 24.9) 
             result.innerHTML = 
-                `Hi <span>${name}</span> your BMI is : <span>${bmi}</span> Noraml`;
+                `Hi <span>${name}</span> your BMI is : <span>${bmi}</span>. <br>
+                This BMI is considered Normal.<br>
+                Maintaining a healthy weight may lower the risk of developing certain health conditions,
+                including cardiovascular disease and type 2 diabetes.
+                 Waist-to-hip ratio, waist-to-height ratio, and body fat percentage measurements can provide a
+                more complete picture of any health risks.`;
         else if (bmi >= 25.0 && bmi < 29.9) 
             result.innerHTML = 
-            `Hi <span>${name}</span> your BMI is : <span>${bmi}</span> Over Weight`;
+            `Hi <span>${name}</span> your BMI is : <span>${bmi}</span>. <br>
+            This BMI is considered Overweight.<br>
+             Being overweight may increase the risk of certain health conditions, including cardiovascular disease,
+              high blood pressure, and type 2 diabetes. Waist-to-hip ratio, waist-to-height ratio, and body fat 
+              percentage measurements can provide a more complete picture of any health risks. A person should 
+              consult with their healthcare provider and consider making lifestyle changes through healthy eating 
+              and fitness to improve their health indicators.
+            `;
 
         else result.innerHTML =
-            `Hi <span>${name}</span> your BMI is : <span>${bmi}</span> Obese`;
+            `Hi <span>${name}</span> your BMI is : <span>${bmi}</span>. <br>
+            This BMI is considered Obese.<br>
+             People with obesity have increased risk of cardiovascular disease, type 2 diabetes, high blood pressure,
+            and other health conditions. Waist-to-hip ratio, waist-to-height ratio, and body fat percentage
+             measurements can provide a more complete picture of any health risks. A person should consult with 
+             their healthcare provider and consider making lifestyle changes through healthy eating and fitness to
+              improve their health indicators.
+            `;
     }
 }
